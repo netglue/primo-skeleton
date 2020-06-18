581cc67291c8f393769a089a9e99dea134c1ab2f
George Steel <george@net-glue.co.uk>
2020-06-18 09:29:49 +0100

    Add server request generator method to base test case

--- test/Integration/EndpointTest.php
+++ test/Integration/EndpointTest.php
@@ -28,7 +28,7 @@ class EndpointTest extends TestCase
 
     public function testHomePageIsAccessible() : void
     {
-        $request = (new ServerRequestFactory())->createServerRequest('GET', '/');
+        $request = $this->serverRequest('/');
         $response = $this->handle($request);
         self::assertResponseHasStatus($response, 200);
     }
@@ -43,21 +43,21 @@ class EndpointTest extends TestCase
         $url = $linkResolver->resolve($document->asLink());
         $this->assertNotNull($url, 'No url could be constructed for the given document');
 
-        $request = (new ServerRequestFactory())->createServerRequest('GET', $url);
+        $request = $this->serverRequest($url);
         $response = $this->handle($request);
         self::assertResponseHasStatus($response, 200);
     }
 
     public function testNotFound() : void
     {
-        $request = (new ServerRequestFactory())->createServerRequest('GET', '/' . uniqid('not_found', false));
+        $request = $this->serverRequest('/' . uniqid('not_found', false));
         $response = $this->handle($request);
         self::assertResponseHasStatus($response, 404);
     }
 
     public function testException() : void
     {
-        $request = (new ServerRequestFactory())->createServerRequest('GET', '/exceptional');
+        $request = $this->serverRequest('/exceptional');
         $response = $this->handle($request);
         self::assertResponseHasStatus($response, 500);
     }
